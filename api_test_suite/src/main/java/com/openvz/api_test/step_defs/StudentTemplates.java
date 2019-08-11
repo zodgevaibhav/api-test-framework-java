f290ea9972a1351abfb5d5aeed8b0c0800321571
Vaibhav Zodge <zodgevaibhav@gmail.com>
2019-08-11 09:57:04 -0700

    Initial commit of API Testing framewok using cucumber, extent report, testNg

--- api_test_suite/src/main/java/com/openvz/api_test/step_defs/StudentTemplates.java
+++ api_test_suite/src/main/java/com/openvz/api_test/step_defs/StudentTemplates.java
@@ -3,14 +3,6 @@ package com.openvz.api_test.step_defs;
 import com.openvz.api_test.template_holder.TemplateUpdator;
 
 public class StudentTemplates extends TemplateUpdator {
-	
-public final static String basicTemplate= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><note>\r\n" + 
-		"  <to>Tove</to>\r\n" + 
-		"  <from>Jani</from>\r\n" +
-		"  <heading>Reminder</heading>\r\n" + 
-		"  <body>Don'tforgetmethisweekend!</body>\r\n" + 
-		"</note>";
-
 @Override
 public String getTemplateDirectory() {
 	return "c:\\vaibhav\\zodge\\xml";
